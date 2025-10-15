e88f99f2034a79ba32885b56514e3a50d9104682
lauto554 <lautarojm185@gmail.com>
2025-10-15 15:51:13 -0300

    importo dotenv y elimino console logs

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,30 @@
-import "colors";
+import dotenv from "dotenv";
 import { initializeDatabase, closeDatabaseConnection } from "./modules/database/db-init";
-import { createServer, startServer } from "./server";
+import { startServer } from "./server";
+import "colors";
 
+dotenv.config();
 const PORT: number = parseInt(process.env.PORT || "3000");
 
 async function startApplication(): Promise<void> {
   try {
-    // 1. Inicializar base de datos
-    await initializeDatabase();
+    startServer(PORT);
 
-    // 2. Crear servidor Express
-    const app = createServer();
-
-    // 3. Iniciar servidor
-    startServer(app, PORT);
+    // await initializeDatabase();
   } catch (error) {
     console.error("Error al iniciar la aplicación:".red, error);
     process.exit(1);
   }
 }
 
-async function gracefulShutdown(): Promise<void> {
-  console.log("Cerrando aplicación...".yellow);
+async function shutDownApplication(): Promise<void> {
   await closeDatabaseConnection();
   process.exit(0);
 }
 
 // Manejo de señales de cierre
-process.on("SIGINT", gracefulShutdown);
-process.on("SIGTERM", gracefulShutdown);
+process.on("SIGINT", shutDownApplication);
+process.on("SIGTERM", shutDownApplication);
 
 // Iniciar la aplicación
 startApplication();
